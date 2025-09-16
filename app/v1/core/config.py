03729c231886e87b64b336bd99e1c0e75ba4d50a
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-16 12:46:28 +0330

    Update config.py

--- app/v1/core/config.py
+++ app/v1/core/config.py
@@ -4,6 +4,11 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+DATABASE_URL = f"postgressql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"

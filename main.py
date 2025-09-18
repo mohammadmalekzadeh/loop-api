88ebad1f056f80a45325f520df62f9d54a1609c4
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-18 14:48:46 +0330

    Update middleware

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ app.include_router(auth.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

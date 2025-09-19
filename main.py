798efff4eb8b13ff8d3f2ff761974edd7f5ba43e
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-19 17:11:48 +0330

    refactor: update middleware and allow origins

--- main.py
+++ main.py
@@ -9,18 +9,13 @@ from app.api.v1.endpoints.request import request
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "https://loop-ui-nu.vercel.app"
-]
-
 app.include_router(auth.router)
 app.include_router(user)
 app.include_router(request.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

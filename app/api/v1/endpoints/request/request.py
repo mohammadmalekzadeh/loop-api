005e432d3cdad5b8ade53e2cd68437447ec08428
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-09-22 18:27:55 +0330

    feat: update change request status and rating system

--- app/api/v1/endpoints/request/request.py
+++ app/api/v1/endpoints/request/request.py
@@ -89,14 +89,40 @@ async def update_request_status(
     if not req:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="Request not found")
 
-    if current_user.role == "vendors" and req.vendors_id != current_user.id:
+    if current_user.role == UserRoleEnum.vendors and req.vendors_id != current_user.id:
         raise HTTPException(status_code= status.HTTP_403_FORBIDDEN, detail="FORBIDDEN")
-    if current_user.role == "customer" and req.user_id != current_user.id:
+    if current_user.role == UserRoleEnum.customer and req.user_id != current_user.id:
         raise HTTPException(status_code= status.HTTP_403_FORBIDDEN, detail="FORBIDDEN")
 
+    product = db.query(Product).filter(Product.id == req.product_id).first()
+
+    product.buy_freq =+ req.count
     req.status = status_value
+
     db.commit()
     db.refresh(req)
+    db.refresh(product)
 
     return {"message": "وضعیت با موفقیت تغییر کرد", "status": req.status}
 
+@router.post("/{request_id}")
+async def postRate(
+        request_id: int,
+        rate: float,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user),
+):
+    request = db.query(Request).filter(Request.id == request_id).first()
+
+    if current_user.id != request.user_id:
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail="UNAUTHORIZED")
+
+    vendors = db.query(Vendors).filter(request.vendors_id == Vendors.id).first()
+    product = db.query(Product).filter(request.product_id == Product.id).first()
+
+    vendors.rate = round((vendors.rate + rate) / 2, 2) if vendors.rate != 0 else rate
+    product.rate = round((product.rate + rate), 2) / 2 if product.rate != 0 else rate
+
+    db.commit()
+    db.refresh(vendors)
+    db.refresh(product)
--- app/models/product.py
+++ app/models/product.py
@@ -12,6 +12,7 @@ class Product(Base):
     price = Column(Integer, nullable=False)
     rate = Column(Float, nullable=True, default=0.0)
     buy_freq = Column(Integer, nullable=True, default=0)
+    is_active = Column(Boolean, default=True)
 
     vendors = relationship("Vendors", back_populates="product")
     request = relationship("Request", back_populates="product")

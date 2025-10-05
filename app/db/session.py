b90e20df007299e3fea665ff14b18f2e54a226a5
Mohammad Malekzadeh <malekmath021@gmail.com>
2025-10-05 13:16:51 +0330

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.core.config import DATABASE_URL, TEST_DB_URL
+from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
